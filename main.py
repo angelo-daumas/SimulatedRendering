1f1b24c6d25fb1949e955f89d803c3352a075bb4
angelo-daumas <42705006+angelo-daumas@users.noreply.github.com>
2021-08-16 15:22:15 -0300

    Implement reading scene from json files

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
+from primitives.primitives import Primitive
+from primitives.polygon import ConvexPolygon
 import numpy as np
 from numpy.typing import NDArray
 
-import scipy as sp
-from matplotlib import patheffects
-import matplotlib.pyplot as plt
 import json
 
-from typing import Sequence, Any, Dict, cast
-from numbers import Number
+from typing import Sequence, Any, Dict, List, Type
 import primitives
 
-def inside(x, y, primitive):
-  """
-  Check if point (x,y) is inside the primitive
-  
-  Args:
-    x (float): horizontal point position
-    y (float): vertical point position
-  Returns:
-    True if (x,y) is inside the primitive, False case contrary
-  """
-  
-  # You should implement your inside test here for all shapes   
-  # for now, it only returns a false test
-
-  return False
-
+from primitives import Circle, Polygon, ConvexPolygon
 class Screen:
     ''' Creates a virtual basic screen
 
@@ -40,34 +23,49 @@ class Screen:
     def __init__(self, gdata:Dict[str, Any]):
         self._width = int(gdata["width"])
         self._height = int(gdata["height"])
-        self._scene = self.preprocess( cast(Sequence[Any], gdata.get("scene") ) )
+        self._scene = self.parse_primitives(gdata["scene"])
 
         # Create white image with R, G, B channels
         self._image = 255 *  np.ones((self._height, self._width, 3), np.uint8) #type: ignore
 
 
-    def preprocess(self, scene:Sequence[Any]) -> Sequence[primitives.Primitive]:
+    def parse_primitives(self, scene:Sequence[Dict[str, Any]]) -> List[primitives.Primitive]:
         ''' 
         '''
-        preprop_scene:Sequence[Any] = []
+        preprop_scene:List[Any] = []
 
         for primitive in scene:
-            preprop_scene.append(primitive)
+            primitive_type:Type[Primitive] = {
+                "circle":Circle, 
+                "polygon":Polygon, 
+                "triangle":ConvexPolygon
+                }[primitive["shape"]]
+            shape = primitive_type.from_dict(primitive)
+            shape.color = primitive["color"]
+            preprop_scene.append(shape)
 
         return preprop_scene
         
     def rasterize(self) -> None:
         ''' Rasterize the primitives along the Screen    
         '''
-
         for primitive in self._scene:
             for w,h in primitive.boundingBox.pixels():
                 if primitive.contains((w+0.5, h+0.5)):  # add 0.5 to get the pixel's  center
                     im_x, im_y = w, self._height - (h + 1)
-                    self._image[im_y, im_x] = (255, 0, 0)
+                    self._image[im_y, im_x] = primitive.color
 
-from primitives import Circle, Polygon
-screen = Screen({"width": 200, "height": 600, "scene": [Circle((100,300), 10.), Polygon([(50,50),(100,50),(100,100), (50,75)])]})
+
+with open('tests/lion.json') as f:
+    data = json.load(f)
+
+# data = {"width": 200, "height": 600, "scene": [Circle((100,300), 10.), ConvexPolygon([(50,50),(100,50),(100,100), (50,75)])]}
+
+screen = Screen(data)
+import time
 from PIL import Image
+start = time.process_time()
 screen.rasterize()
+end = time.process_time()
+print(f"Finished rasterizing. Took {end-start} seconds.")
 img = Image.fromarray(screen._image).show()
